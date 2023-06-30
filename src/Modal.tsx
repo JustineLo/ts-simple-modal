c7b903eadcc819b629266119fe5fd339395f7a04
Justine Lo <justine@ecotree.fr>
2023-06-30 20:33:23 +0200

    add JSdoc

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -7,6 +7,17 @@ interface ModalProps {
     children: React.ReactNode;
 }
 
+/*
+* @component
+*
+* @param {Object} props - The component props.
+* @param {boolean} props.show - Determines whether the modal should be displayed.
+* @param {Function} props.onClose - The function to be called when the modal is closed.
+* @param {React.ReactNode} props.children - The content to be rendered inside the modal.
+*
+* @returns {JSX.Element|null} The Modal component.
+*/
+
 function Modal ({ show, onClose, children }: ModalProps){
   if (!show) {
     return null;
